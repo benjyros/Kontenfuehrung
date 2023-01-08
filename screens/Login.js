5517731487ad4cf3bfb8f47d3b6a8a1824a4d239
benjyros <benny.peterhans@gmail.com>
2023-01-08 18:55:22 +0100

    added import for useeffect and -state

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, ScrollView, Text, TextInput, TouchableOpacity, View, InputAccessoryView } from 'react-native';
 
