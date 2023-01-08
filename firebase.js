8138fb030a76faf11d58a628cea07ab8964c45e1
benjyros <benny.peterhans@gmail.com>
2023-01-08 18:36:00 +0100

    added login page with firebase configuration

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBZAj6XDkymOwb6PNSAng4UlZSivRNbHeM",
+    authDomain: "kontenfuehrung.firebaseapp.com",
+    projectId: "kontenfuehrung",
+    storageBucket: "kontenfuehrung.appspot.com",
+    messagingSenderId: "841043579781",
+    appId: "1:841043579781:web:8a54e9c56af5135c78aaec",
+    measurementId: "G-Y6C3DHCFEY"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+const auth = getAuth(app)
+
+export { auth, storage, storageRef }
--- screens/Login.js
+++ screens/Login.js
@@ -1,41 +1,72 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, ScrollView, Text, TextInput, TouchableOpacity, View, InputAccessoryView } from 'react-native';
+import { auth } from "../firebase";
+import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 
-export default function Login ({ navigation }) {
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigation.replace("Home");
+      } else {
+        setEmail("");
+        setPassword("");
+      }
+      return unsubscribe;
+    });
+  }, []);
+
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+      })
+      .catch((error) => {
+        alert("Dieses Konto existiert nicht oder die Angaben sind inkorrekt.");
+      });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-			<Button
-        title="To login"
-        onPress={() =>
-          navigation.navigate('Login')
-        }
-    	/>
-      <Button
-        title="To registration"
-        onPress={() =>
-          navigation.navigate('Registration')
-        }
-    	/>
-			<Button
-        title="To home"
-        onPress={() =>
-          navigation.navigate('Home')
-        }
-    	/>
-			<Button
-        title="To create payment"
-        onPress={() =>
-          navigation.navigate('CreatePayment')
-        }
-    	/>
-			<Button
-        title="To create account"
-        onPress={() =>
-          navigation.navigate('CreateAccount')
-        }
-    	/>
+    <View style={indexStyle.container}>
+      <View style={indexStyle.header}>
+        <Text style={indexStyle.title}>Kontenführung</Text>
+      </View>
+      <ScrollView
+        scrollEnabled={false}
+        style={indexStyle.main}
+        keyboardShouldPersistTaps='handled'
+      >
+        <View style={styles.container}>
+          <TextInput
+            style={inputView.textInput}
+            onChangeText={(email) => setEmail(email)}
+            placeholder="E-Mail"
+          />
+          <TextInput
+            style={inputView.textInput}
+            onChangeText={(password) => setPassword(password)}
+            placeholder="Passwort"
+          />
+          <TouchableOpacity
+            style={textLink.textLinkViewRight}
+            onPress={() =>
+              navigation.navigate("Registration")
+            }
+          >
+            <Text style={textLink.textLink}>Registrieren</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={buttonView.button}
+            onPress={handleSignIn}
+          >
+            <Text style={buttonView.buttonText}>Anmelden</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -43,9 +74,8 @@ export default function Login ({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: "30%",
+    alignItems: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
