a1557a2a8df963d16d9ea002bbb9a7266f164bbc
benjyros <benny.peterhans@gmail.com>
2023-01-09 17:05:49 +0100

    added comboboxes

--- screens/AccountTransfer.js
+++ screens/AccountTransfer.js
@@ -1,105 +1,183 @@
 import React, { useEffect, useState } from "react";
 import { Picker } from '@react-native-picker/picker';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
-import loginStyle from './styles/loginStyle';
 import buttonView from './styles/buttonView';
-import inputView from './styles/inputView';
 import textLink from './styles/textLink';
 
 import { auth, firestore } from "../firebase";
-import { doc, getDoc, collection, setDoc, getDocs } from "firebase/firestore";
+import { doc, getDoc, collection, setDoc, getDocs, query, where } from "firebase/firestore";
 
 
 export default function Registration({ navigation }) {
-	const [selectedValue, setSelectedValue] = useState("java");
-	const [debitAcc, setDebitAcc] = useState("java");
+	const [debitAcc, setDebitAcc] = useState("");
 	const [creditAcc, setCreditAcc] = useState("");
 	const [amount, setAmount] = useState("");
 
+	const [openDebitAcc, setOpenDebitAcc] = useState(false);
+	const [openCreditAcc, setOpenCreditAcc] = useState(false);
+	const [accounts, setAccounts] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			// Defining all types of accounts
+			var types = ["Privatkonto", "Sparkonto"];
+			// Defining array for all accounts
+			var accounts = [];
+			//Looping through all types of accounts
+			for (let i = 0; i < types.length; i++) {
+				// Query accounts where {types[i]}
+				const q = query(collection(firestore, "users", auth.currentUser.uid, "accounts"), where("type", "==", types[i]));
+				// Get documents of query
+				const querySnapshot = await getDocs(q);
+				// Loop through all documents
+				querySnapshot.forEach((doc) => {
+					// Get specific datas out of the document
+					const getType = () => {
+						if (i === 0) {
+							return doc.data().type;
+						} else {
+							return doc.data().type + " " + accounts.length;
+						}
+					}
+					const newAccount = {
+						label: getType() + ": " + doc.data().iban + " - " + doc.data().balance + " CHF",
+						value: doc.data().iban
+					};
+					// Put datas into array for all accounts
+					accounts[accounts.length] = newAccount;
+				});
+			}
+			// Set useState with the accounts
+			setAccounts(accounts);
+		};
+		fetchData();
+	}, []);
+
+
 	const transfer = () => {
-		if(amount != ""){
+		if (debitAcc === creditAcc) {
+			alert("Sie können nicht auf das gleiche Konto transferieren.");
+		} else if (debitAcc === "" || creditAcc === "") {
+			alert("Bitte geben Sie die Kontos an.");
+		} else if (amount != "") {
 			navigation.replace("Home");
-		}else{
+		} else {
 			alert("Bitte geben Sie einen Betrag ein.");
 		}
 	}
 
 	return (
-		<View style={loginStyle.container}>
-			<ScrollView
-				scrollEnabled={false}
-				style={loginStyle.container}
-				keyboardShouldPersistTaps='handled'
-			>
-				<View style={loginStyle.headerRegistration}>
-					<Text style={loginStyle.title}>Registrierung</Text>
-				</View>
-			</ScrollView>
-			<ScrollView
-				scrollEnabled={false}
-				style={styles.mainContainer}
-				keyboardShouldPersistTaps='handled'
-			>
-				<View style={styles.container}>
-					<View style={styles.pickers}>
-						<Picker
-							selectedValue={debitAcc}
-							style={{ height: 50, width: "75%" }}
-							onValueChange={(itemValue, itemIndex) => setDebitAcc(itemValue)}
-						>
-							<Picker.Item label="Java" value="java" />
-							<Picker.Item label="JavaScript" value="js" />
-							<Picker.Item label="Python" value="python" />
-							<Picker.Item label="C++" value="cpp" />
-							<Picker.Item label="C#" value="csharp" />
-						</Picker>
-						<Picker
-							selectedValue={creditAcc}
-							style={{ height: 25, width: "75%", marginTop: 50, }}
-							onValueChange={(itemValue, itemIndex) => setCreditAcc(itemValue)}
-						>
-							<Picker.Item label="Java" value="java" />
-							<Picker.Item label="JavaScript" value="js" />
-							<Picker.Item label="Python" value="python" />
-							<Picker.Item label="C++" value="cpp" />
-							<Picker.Item label="C#" value="csharp" />
-						</Picker>
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<ScrollView
+					scrollEnabled={false}
+					keyboardShouldPersistTaps='handled'
+				>
+					<View style={styles.headerRegistration}>
+						<Text style={styles.title}>Kontoübertrag</Text>
 					</View>
-
-					<TextInput
-						style={inputView.textInput}
-						onChangeText={(amount) => setAmount(amount)}
-						placeholder="z.B. 200"
-					/>
-					<TouchableOpacity
-						style={[buttonView.button, { marginTop: 50 }]}
-						onPress={transfer}
-					>
-						<Text style={buttonView.buttonText}>Ausführen</Text>
-					</TouchableOpacity>
-					<TouchableOpacity
-						style={textLink.container}
-						onPress={() =>
-							navigation.replace("Login")
-						}
-					>
-						<Text style={textLink.textLinkLeft}>&lt; Abbrechen</Text>
-					</TouchableOpacity>
-				</View>
-			</ScrollView>
-			<StatusBar style="auto" />
+				</ScrollView>
+			</View>
+			<View style={styles.main}>
+				<ScrollView
+					scrollEnabled={false}
+					keyboardShouldPersistTaps='handled'
+				>
+					<View style={styles.container}>
+						<View style={styles.pickers}>
+							<View style={styles.picker}>
+								<Text>Belastungskonto</Text>
+								<DropDownPicker
+									open={openDebitAcc}
+									value={debitAcc}
+									items={accounts}
+									setOpen={setOpenDebitAcc}
+									setValue={setDebitAcc}
+									setItems={setAccounts}
+									maxHeight={75}
+									textStyle={{
+										fontSize: 10
+									}}
+								/>
+							</View>
+							<View style={styles.picker}>
+								<Text>Gutschriftskonto</Text>
+								<DropDownPicker
+									open={openCreditAcc}
+									value={creditAcc}
+									items={accounts}
+									setOpen={setOpenCreditAcc}
+									setValue={setCreditAcc}
+									setItems={setAccounts}
+									maxHeight={75}
+									textStyle={{
+										fontSize: 10
+									}}
+								/>
+							</View>
+						</View>
+						<View style={styles.subcontent}>
+							<View style={styles.amount}>
+								<Text>Betrag</Text>
+								<TextInput
+									style={styles.textInput}
+									onChangeText={(amount) => setAmount(amount)}
+									placeholder="z.B. 200"
+								/>
+								<Text>CHF</Text>
+							</View>
+							<TouchableOpacity
+								style={[buttonView.button, { marginTop: 50 }]}
+								onPress={transfer}
+							>
+								<Text style={buttonView.buttonText}>Ausführen</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								style={textLink.container}
+								onPress={() =>
+									navigation.replace("Login")
+								}
+							>
+								<Text style={textLink.textLinkLeft}>&lt; Abbrechen</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
+				</ScrollView>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		alignItems: "center",
 		flex: 1,
+		width: "100%",
+	},
+	header: {
+		flex: 1,
+		backgroundColor: "red",
+		width: "100%",
+	},
+	main: {
+		flex: 3,
+		backgroundColor: "blue",
+		width: "100%",
 	},
 	pickers: {
-		justifyContent: "center",
+		flex: 1,
+		backgroundColor: "orange",
+		height: 400,
+	},
+	picker: {
+		margin: 50,
+	},
+	subcontent: {
+		flex: 1,
+		backgroundColor: "yellow"
+	},
+	amount: {
+		flexDirection: "row",
 	}
 });
